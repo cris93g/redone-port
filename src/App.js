9eaee8fb79f5b7d628e8cc19df19e01ba88eaee8
cris93g <37388115+cris93g@users.noreply.github.com>
2019-03-14 05:58:10 -0500

    check stuff

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { HashRouter as Router } from "react-router-dom";
 import routes from "./routes";
-import Nav from "./components/nav/Nav";
+import Nav from "./components/nav/nav";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div className="App">
-					<Nav />
-
-					{routes}
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -3,29 +3,29 @@ import "./Nav.css";
 import { Link } from "react-router-dom";
 import DarkModeToggle from "../nightMode/DarkModeToggle";
 export default class Nav extends Component {
-	render() {
-		return (
-			<div className="Nav">
-				<Link to="/">
-					<div className="leftN">Cristian Gonzalez</div>
-				</Link>
-				<DarkModeToggle />
-				<div className="middleN" />
-				<div className="rightN">
-					<Link to="/">
-						<div className="navItem">About</div>
-					</Link>{" "}
-					<Link to="/Projects">
-						<div className="navItem">Projects</div>
-					</Link>
-					<Link to="/Education">
-						<div className="navItem">Technologies</div>
-					</Link>
-					<Link to="/Contact">
-						<div className="navItem">Contact</div>
-					</Link>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="Nav">
+        <Link to="/">
+          <div className="leftN">Cristian Gonzalez</div>
+        </Link>
+        <DarkModeToggle />
+        <div className="middleN" />
+        <div className="rightN">
+          <Link to="/">
+            <div className="navItem">About</div>
+          </Link>{" "}
+          <Link to="/Projects">
+            <div className="navItem">Projects</div>
+          </Link>
+          <Link to="/Education">
+            <div className="navItem">Technologies</div>
+          </Link>
+          <Link to="/Contact">
+            <div className="navItem">Contact</div>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
